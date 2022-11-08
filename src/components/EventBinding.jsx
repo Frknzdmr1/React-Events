52ac77b8f0fade47de6346ca1148ed97ba23301a
Frknzdmr1 <ozdemirf97@icloud.com>
2022-11-08 08:30:05 -0500

    v2

--- src/components/EventBinding.jsx
+++ src/components/EventBinding.jsx
@@ -2,17 +2,28 @@ import React, { Component } from 'react'
 
 class EventBinding extends Component {
 
-    state = {message: 'Hello World'}
+
+  // updating the message with state method
+
+  constructor(props) {
+    super(props);
+    this.state = {message: "Nasilsin"}
+    this.clickHandler = this.clickHandler.bind(this);
+  }
+
+    // state = {message: 'Nasilsin ?'}
 
     clickHandler() {
-        this.setState({message: 'Bye World'})
+        this.setState({message: 'Sanane doktormusun aq'})
     }
 
   render() {
     return (
       <>
        <h1> {this.state.message}</h1>
-       <button onClick ={this.clickHandler }>Click me</button>
+       {/* <button onClick ={this.clickHandler.bind(this)}> Merakini gider </button> */}
+       {/* <button onClick ={() => this.clickHandler('Sanabe doktormusun') }> Merakini gider </button> */}
+       <button onClick ={ this.clickHandler }> Merakini gider </button>
       </>
     )
   }
